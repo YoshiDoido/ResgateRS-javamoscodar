1aa964ef45eb85145c9a20cb9b688a672d0c8e56
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-05-23 16:22:02 -0300

    Mensagem de erro corrigida

--- src/main/java/uol/compass/domain/exception/AbrigoNaoEncontradoException.java
+++ src/main/java/uol/compass/domain/exception/AbrigoNaoEncontradoException.java
@@ -3,10 +3,10 @@ package uol.compass.domain.exception;
 public class AbrigoNaoEncontradoException extends BusinessException {
 
     public AbrigoNaoEncontradoException(Integer id) {
-        super(String.format("Centro de Distribuição de código %d não foi encontrado", id));
+        super(String.format("Abrigo de código %d não foi encontrado", id));
     }
 
     public AbrigoNaoEncontradoException(Integer id, Throwable cause) {
-        super(String.format("Centro de Distribuição de código %d não foi encontrado", id), cause);
+        super(String.format("Abrigo de código %d não foi encontrado", id), cause);
     }
 }
